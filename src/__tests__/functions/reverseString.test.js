29e20ad165788965f97425a3c292ec93f9818275
Maschenko Sofia <m.sofia05@mail.ru>
2024-03-23 18:29:20 +0300

    fixed tests

--- src/__tests__/functions/reverseString.test.js
+++ src/__tests__/functions/reverseString.test.js
@@ -4,4 +4,19 @@ describe("Reverse string function test", () => {
     it("should return the reversed string", () => {
         expect(reverseString("hello")).toBe("olleh");
     });
+    it("should return empty string", () => {
+        expect(reverseString("")).toBe("");
+    });
+    it("should return the reversed string with whitespace", () => {
+        expect(reverseString("привет мир")).toBe("рим тевирп");
+    });
+    it("should return the reversed string with different case", () => {
+        expect(reverseString("ПрИвЕт")).toBe("тЕвИрП");
+    });
+    it("should return null", () => {
+        expect(reverseString(null)).toBe(null);
+    });
+    it("should return undefined", () => {
+        expect(reverseString(undefined)).toBe(undefined);
+    });
 });
--- src/functions/reverseString.js
+++ src/functions/reverseString.js
@@ -1,4 +1,7 @@
 export function reverseString(str) {
+   if (str === null) return null;
+   if (str == undefined) return undefined;
+
    let reversedString = '';
    for (let i = str.length - 1; i >= 0; i--) {
       reversedString += str[i];
